8393163d96e9686ea97c871bb0713e0c3db3ec0b
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-08-30 14:07:58 +0200

    completed exercise 2 of math

--- js-complete-course/02-maths/02-calculator-two/script.js
+++ js-complete-course/02-maths/02-calculator-two/script.js
@@ -9,11 +9,34 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-    // to get the value of an input: document.getElementById("element-id").value
+(function () {
+    var x = parseInt(document.getElementById("op-one").value);
+    var y = parseInt(document.getElementById("op-two").value);
 
     var performOperation = function(operation) {
-        // perform the operation
+        switch (operation){
+            case 'addition':
+                var resultAdd= x+y;
+                alert("the result is "+resultAdd);
+
+                break;
+
+            case 'substraction':
+                var resultSub= x-y;
+                alert("the result is "+resultSub);
+                break;
+            case 'multiplication':
+;
+                var resultMultiply= x*y;
+                alert("the result is "+resultMultiply);
+                break;
+            case 'division':
+
+                var resultDivide= x/y;
+                alert("the result is "+resultDivide);
+                break;
+
+        }
     };
 
     Array.from(document.querySelectorAll("button.operator")).forEach(function($btn) {
