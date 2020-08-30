ded7bc666d5b3c57b1328aa11c5f09c71294153d
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-08-30 16:43:42 +0200

    Finished exercise 5 of maths

--- js-complete-course/02-maths/05-factorial/script.js
+++ js-complete-course/02-maths/05-factorial/script.js
@@ -10,12 +10,19 @@
 // You will have time to focus on it later.
 
 (function() {
-    
+
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
 
-        // your code here
+        var  input = document.getElementById("number").value;
+        document.getElementById("run").addEventListener("click", function() {
+
+            for (var i = input - 1; i >= 1; i--) {
+                input *= i;
+            }
+            alert(input);
+        })
 
     });
 
