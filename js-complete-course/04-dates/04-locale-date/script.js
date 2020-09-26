324760b4109931ff3b65336c1a9949fff49d9eb5
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-26 23:04:36 +0200

    finished exercise 4 of dates

--- js-complete-course/04-dates/04-locale-date/script.js
+++ js-complete-course/04-dates/04-locale-date/script.js
@@ -13,6 +13,42 @@
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
+    var d = new Date();
+    var todayYear = d.getFullYear();
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    var todayMonth = month[d.getMonth()];
+    var todayDate = d.getDate();
+    var todayHour= d.getHours();
+    var todayMin= d.getMinutes();
+
+
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var day= weekday[d.getDay()];
+    const event = new Date(d);
+
+    console.log(event.toString());
+
+
+    document.getElementById("target").innerHTML = day+" "+ todayDate+" " +todayMonth+ " "+todayYear+", "+ todayHour+"h"+todayMin;
+
 
 })();
