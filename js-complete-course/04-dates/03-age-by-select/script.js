84b30deb6b4f8759c5257cb335bf96ff2575558b
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-26 22:52:23 +0200

    finished exercise 3 of dates, made it that even the age is correct when it's the same month

--- js-complete-course/04-dates/03-age-by-select/script.js
+++ js-complete-course/04-dates/03-age-by-select/script.js
@@ -10,7 +10,52 @@
 // You will have time to focus on it later.
 
 (function() {
+    var age;
+    document.getElementById("run").addEventListener("click", function() {
+        //getting the birthday of the input
+        let dobDay = document.getElementById("dob-day").value;
+        let dobMonth = document.getElementById("dob-month").value;
+        let dobYear = document.getElementById("dob-year").value;
 
-    // your code here
+        var d = new Date();
+
+        //Gets the values of today
+        var dYear = d.getFullYear();
+        var dMonth = d.getMonth();
+        var dDate = d.getDate();
+
+        //basically calculating the approximate age by first subtracting date of birth year by the year of today
+        var yearsDiff = dYear - dobYear;
+        //checking if the month is less than the current one
+        if ( dMonth < dobMonth )
+        {
+            age = yearsDiff - 1;
+            if ( dDate <= dobDay )
+            {
+                age = yearsDiff - 1;
+            }
+            else
+            {
+                age = yearsDiff;
+            }
+        }
+        else if ( dMonth >= dobMonth )
+        {
+            age = yearsDiff ;
+            if ( dDate <= dobDay )
+            {
+                age = yearsDiff - 1;
+            }
+            else
+            {
+                age = yearsDiff;
+            }
+        }
+
+
+        alert("You are "+age+ " years old");
+
+
+    })
 
 })();
